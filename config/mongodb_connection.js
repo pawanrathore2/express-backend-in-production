2b8e9c12634f0253d0dc82ec7332e589b9342a53
Pawan rathore <pawanrathor259@gmail.com>
2025-03-31 12:14:52 +0530

    31/03/25

--- config/mongodb_connection.js
+++ config/mongodb_connection.js
@@ -2,7 +2,8 @@
 import mongoose from 'mongoose';
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGOOS_URL);
+        const conn = await mongoose.connect(process.env.LOCAL_MONGODB
+);
         console.log(`MongoDB Connected At: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
